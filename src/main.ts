bf96f7e956467f97085fd676249dbdb3269795d9
suyh <su787910081@163.com>
2025-01-05 18:54:16 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
 
-import {createPinia} from "pinia";
-
-let app = createApp(App);
-let pinia = createPinia();
-app.use(pinia)
-app.mount('#app')
+createApp(App).mount('#app')
